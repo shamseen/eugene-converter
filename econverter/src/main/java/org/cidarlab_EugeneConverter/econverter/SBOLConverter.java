52774d919a6763cb88040e54749671a7aaedba3e
Shamseen <shamseen.c@gmail.com>
2017-06-16 17:32:32 -0400

    dependencies and exceptions

--- econverter/src/main/java/org/cidarlab_EugeneConverter/econverter/SBOLConverter.java
+++ econverter/src/main/java/org/cidarlab_EugeneConverter/econverter/SBOLConverter.java
@@ -1,5 +1,7 @@
 package org.cidarlab_EugeneConverter.econverter;
 
+import org.adaptor.SynBioHubAdaptor;
+import org.cidarlab_EugeneConverter.econverter.dom.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,13 +14,25 @@ import org.sbolstandard.core2.SBOLDocument;
 import org.sbolstandard.core2.SBOLValidate;
 import org.sbolstandard.core2.SBOLValidationException;
 import org.sbolstandard.core2.SBOLWriter;
-import org.adaptor.SynBioHubAdaptor;
+import org.sbolstandard.core2.Component;
+import org.sbolstandard.core2.ComponentDefinition;
+import org.sbolstandard.core2.OrientationType;
+import org.sbolstandard.core2.Sequence;
+import org.sbolstandard.core2.SequenceAnnotation;
+import org.sbolstandard.core2.SequenceOntology;
+
 import org.synbiohub.frontend.SynBioHubException;
 
 
 public class SBOLConverter 
 {
 	private SynBioHubAdaptor instance; //connection to symbiohub db
+	
+	// Takes in Part and a doc to add conversions. Outputs modified doc.
+	public static SBOLDocument convertPartToComponent(Part part, SBOLDocument doc) throws BOLValidationException, MalformedURLException, URISyntaxException
+	{
+		
+	}
 	
 	
 	 @Before
